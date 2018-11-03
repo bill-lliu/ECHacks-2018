9e4ef0e31d2f4871f2b6720e5394306de8f669e4
Matt <alpmatthew@gmail.com>
2018-11-03 19:40:10 -0400

    Update config.py
    Co-Authored-By: Nicole Streltsov <streltsovnicole@gmail.com>
    Co-Authored-By: Bill Liu <penguinofwinter@users.noreply.github.com>
    Co-Authored-By: guymorgenshtern <guymorgenshtern@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-
 import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data")
-DATABASE_URL = "sqlite:///" + os.path.join(DATA_FOLDER, "webapp.db")
