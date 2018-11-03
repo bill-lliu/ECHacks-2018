47974938bc58e8de9d9bcd2440d1c8c0ec075884
Guy Morgenshtern <guymorg17@gmail.com>
2018-11-03 14:53:34 -0400

    Logic

--- run_server.py
+++ run_server.py
@@ -1,10 +1,11 @@
 from server.app import app, db
-from server.models import Refugee
+from server.models import Refugee, HostFamily
 
 
 def populate_database():
     db.create_all()
     db.session.add(Refugee(refugee_id=1, name="Guy Morgenshtern", age=17, primary_language="English", secondary_language="Also English", family_size = 1, previous_occupation="Food eater"))
+    db.session.add(HostFamily(host_id=1, name ="Refugee 1", age=24, primary_language="English", secondary_language="", available_space=3, family_size=2, occupation="Electrician", salary=70000, location="Toronto"))
     db.session.commit()
 
 
