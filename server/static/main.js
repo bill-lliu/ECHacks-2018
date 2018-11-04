5c569d9d8e3834c3268872af35da3204f17f9520
Guy Morgenshtern <guymorg17@gmail.com>
2018-11-04 08:33:54 -0500

    API KEY

--- server/static/main.js
+++ server/static/main.js
@@ -1,11 +1,11 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox.streets',
-//     accessToken: 'your.mapbox.access.token'
-// }).addTo(mymap);
+ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+     maxZoom: 18,
+     id: 'mapbox.streets',
+     accessToken: 'pk.eyJ1IjoiZ3V5bW9yZyIsImEiOiJjam8yd25vbGwwcjRwM3FxaTk5Z3EyZjBnIn0.BoDV84KaHRnA3nl4XTbH9w'
+ }).addTo(mymap);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
