0c49c21687f97d66ae836e437c1d1e249db7dddd
penguinofwinter <winterbirdwinterbird@gmail.com>
2018-11-04 00:48:19 -0400

    Researched Locations & Global Positions
    2 hours of work crappo lmao

--- server/models.py
+++ server/models.py
@@ -5,7 +5,6 @@ class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1024), nullable=False)
     description = db.Column(db.String(4096), nullable=False)
-    location = db.Column(db.String(1024), unique=True, nullable=False)
     long = db.Column(db.Float, nullable=False)
     lat = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(140), nullable=False)
